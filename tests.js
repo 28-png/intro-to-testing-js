339ad17075730bd2693340cd07cb5d99463793a2
Matt Murphy <murphy.d.matthew28@gmail.com>
2019-12-06 07:15:05 -0600

    updated

--- tests.js
+++ tests.js
@@ -1,9 +1,21 @@
+describe('name for test', function() {
+   it('this is the label for this test', function() {
+       expect(test).toBeDefined(false);
+       });
+       it('to be', function() {
+          expect(test).toBe(true);
+   });
+   it('not to be', function() {
+       expect(test).not.toBe(false);
+   });
+});
+
 // Unit tests for the helloWorld function
 describe('helloWorld', function() {
     it('should be a defined function', function() {
       expect(typeof helloWorld).toBe('function');
     });
-    it('should return a string when called', function() {
+     it('should return a string when called', function() {
       expect(typeof helloWorld()).toBe("string");
     });
     it('should return the string "Hello, World!" when executed', function() {
